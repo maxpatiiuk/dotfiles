e45b560f8786bf62776d9bdae9a36b7cadc77f05
Max Patiiuk <max@patii.uk>
2021-04-11 16:44:15 +0300

    Create new git aliases

--- zsh/.oh-my-zsh/git.zsh
+++ zsh/.oh-my-zsh/git.zsh
@@ -1,12 +1,22 @@
-alias gg="git log --graph --oneline --pretty=format:\"%C(yellow)%h %Cgreen%an %Cblue%ar %Cred%s%Creset\""
+# potentially destructive commands like rm, reset, merge, rebase and
+# push do not have aliases to prevent accidents
+
+# also, I know that git has a native support for aliases, but those
+# aren't short enough for me
+
+alias gg="git log --graph --oneline --pretty=format:\"%C(yellow)%h %Cgreen%an %Cblue%ar %Cred%s%Creset %C(auto)%d%Creset\""
 alias gl="git log --graph --stat"
 alias g-="git switch -"
 alias gs="git status"
 alias gr="git status && git pull --rebase && git status"
 alias gf="git fetch && git status"
-alias gd="git diff"
-alias gdc="git diff --cached"
+alias gdw="git diff"
+alias gd="git diff --color-words"
+alias gdcw="git diff --cached"
+alias gdc="git diff --color-words --cached"
 alias gi="git add --interactive"
 alias gc="git commit"
 alias gca="git commit --amend"
+alias gb="git branch -vv"
+alias gba="git branch -vv --all"
 
