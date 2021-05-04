6be7ba92c1d9ff98ddcd56811c673af911dd7140
Max Patiiuk <max@patii.uk>
2021-05-04 17:03:18 +0300

    Update zsh aliases

--- zsh/.oh-my-zsh/git.zsh
+++ zsh/.oh-my-zsh/git.zsh
@@ -8,6 +8,7 @@
 alias gg="git log --graph --oneline --pretty=format:\"%C(yellow)%h %Cgreen%an %Cblue%ar %Cred%s%Creset %C(auto)%d%Creset\""
 alias gl="git log --graph --stat"
 alias g-="git switch -"
+alias g--="git switch"
 alias gs="git status"
 alias gr="git status && git pull --rebase && git status"
 alias gf="git fetch && git status"
--- zsh/.oh-my-zsh/programs.zsh
+++ zsh/.oh-my-zsh/programs.zsh
@@ -1,6 +1,13 @@
 alias clipboard="python3 ~/site/git/code_share/Python/clipboard/clip_board.py"
 alias tts="python3 ~/site/python/tts/run.py"
-alias yt="youtube-dl --exec \"afplay /System/Library/Sounds/Funk.aiff #\""
 alias g="python3 ~/site/git/code_share/Python/github/github.py"
 # `f` because `cc`, `dc`, `dd` and `ss` is already taken :)
 alias f="python3 ~/site/git/dir-explorer/dir-explorer/main.py"
+alias notify="afplay /System/Library/Sounds/Funk.aiff"
+alias yt='docker run \
+  --rm -i \
+  -e PGID=$(id -g) \
+  -e PUID=$(id -u) \
+  -v "$(pwd)":/workdir:rw \
+  mikenye/youtube-dl \
+  -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"'
