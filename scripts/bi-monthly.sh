cbb24695fa000d14745bf8ae9e4ce35004380646
Max Patiiuk <max@patii.uk>
2021-06-07 04:41:45 +0300

    Extract pre-commit tools into a separate repository

--- scripts/bi-monthly.sh
+++ scripts/bi-monthly.sh
@@ -2,14 +2,18 @@
 
 echo A bi-monthly launchctl task that does various maintenance tasks
 
-echo Update pre-commit hooks
+echo Updating pre-commit hooks
 /usr/local/bin/pre-commit autoupdate --config ~/site/git/dotfiles/git/.pre-commit-config.yaml
 /usr/local/bin/pre-commit gc
 
-echo Maintain Homebrew
+echo Maintaining Homebrew
 /usr/local/bin/brew update
 /usr/local/bin/brew upgrade
 /usr/local/bin/brew cleanup
 /usr/local/bin/brew doctor
+
+echo Calling additional scripts
+pre_commit_tools_dir="${HOME}/site/git/pre-commit-tools"
+"${pre_commit_tools_dir}/venv/bin/python" "${pre_commit_tools_dir}/pre-commit-tools/update_hooks.py"
 
 echo Exit
