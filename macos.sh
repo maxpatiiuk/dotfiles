d99d0fc371d79b079ae8b7cd8498e087afd68a27
Max Patiiuk <max@patii.uk>
2023-08-27 09:53:41 -0700

    feat: Enable key repeat in VS Code

--- macos.sh
+++ macos.sh
@@ -331,6 +331,24 @@ defaults write com.apple.commerce AutoUpdate -bool true
 defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
 
 ###############################################################################
+# VS Code                                                                     #
+###############################################################################
+#
+# For VSCode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# For VSCode Insiders
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+
+# For VSCodium
+defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false
+
+# To enable global key-repeat
+# this is helpful if you're using Vim in a PWA like code-server
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+
+###############################################################################
 # Photos                                                                      #
 ###############################################################################
 
