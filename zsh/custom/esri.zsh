75d50dea24a01063cac3a382e8d567d94b2a5b23
Max Patiiuk <max@patii.uk>
2023-11-30 11:04:03 -0800

    feat(zsh): replace codemod shell aliases with npm scripts

--- zsh/custom/esri.zsh
+++ zsh/custom/esri.zsh
@@ -1,6 +1,3 @@
 alias se="cd ~/site/esri"
+alias sm="cd ~/site/esri/arcgis-web-components/packages/map-components/src/migration"
 alias e="cd ~/site/esri/arcgis-js-api-4"
-
-# Meant to be called from the ./migration directory
-alias migrate-widget="node --no-warnings=ExperimentalWarning --loader ts-node/esm/transpile-only ./index.mts"
-alias debug-migrate-widget="node --inspect-brk --no-warnings=ExperimentalWarning --loader ts-node/esm/transpile-only ./index.mts"
