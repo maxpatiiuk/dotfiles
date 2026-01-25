1b8a3fedf43eb0dd509688ff855cb87637e6f5b0
Max Patiiuk <max@patii.uk>
2026-01-24 17:28:18 -0800

    feat(macos): reduce animations

--- macos.sh
+++ macos.sh
@@ -121,6 +121,35 @@ defaults write com.apple.dock show-recents -bool false
 # Disable the Launchpad gesture (pinch with thumb and three fingers)
 defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
 
+# Disable animations to make system usable
+defaults write -g NSScrollViewRubberbanding -int 0
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+defaults write -g NSScrollAnimationEnabled -bool false
+defaults write -g NSWindowResizeTime -float 0.001
+defaults write -g QLPanelAnimationDuration -float 0
+defaults write -g NSDocumentRevisionsWindowTransformAnimation -bool false
+defaults write -g NSToolbarFullScreenAnimationDuration -float 0
+defaults write -g NSBrowserColumnAnimationSpeedMultiplier -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock autohide-delay -float 0.1
+defaults write com.apple.dock expose-animation-duration -float 0
+defaults write com.apple.dock springboard-show-duration -float 0
+defaults write com.apple.dock springboard-hide-duration -float 0
+defaults write com.apple.dock springboard-page-duration -float 0
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.Mail DisableSendAnimations -bool true
+defaults write com.apple.Mail DisableReplyAnimations -bool true
+defaults write NSGlobalDomain NSWindowResizeTime .001
+defaults write com.apple.dock expose-animation-duration -int 0
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# Keep desktop empty of distractions
+defaults write com.apple.screencapture location ~/Downloads
+
+# Speed up key repeat
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
 # Hot corners
 # Possible values:
 #  0: no-op
